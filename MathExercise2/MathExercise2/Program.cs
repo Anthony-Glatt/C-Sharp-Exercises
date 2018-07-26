16e525b9058ce3f45a163e34be439445dc508d19
Anthony-Glatt <40805126+Anthony-Glatt@users.noreply.github.com>
2018-07-26 16:25:09 -0700

    Math Exercise 2
    - Calculates 2 annual salaries and determines if person 1 makes more than person 2

---
+++ MathExercise2/MathExercise2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+
+namespace MathExercise2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Anonymous Income Comparison Program");
+            //Person 1 Info
+            Console.WriteLine();
+            Console.WriteLine("Person 1");
+            Console.WriteLine("What is your hourly rate?");
+            float person1Rate = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("How many hours do you work per week?");
+            float person1Hours = Convert.ToSingle(Console.ReadLine());
+            //Person 2 Info
+            Console.WriteLine();
+            Console.WriteLine("Person 2");
+            Console.WriteLine("What is your hourly rate?");
+            float person2Rate = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("How many hours do you work per week?");
+            float person2Hours = Convert.ToSingle(Console.ReadLine());
+            //Person 1 Annual Salary
+            Console.WriteLine();
+            float person1Annual = person1Rate * person1Hours * 52;
+            Console.WriteLine("Annual salary of Person 1: $" + person1Annual);
+            //Person 2 Annual Salary
+            Console.WriteLine();
+            float person2Annual = person2Rate * person2Hours * 52;
+            Console.WriteLine("Annual salary of Person 2: $" + person2Annual);
+            //Does person 1 make more?
+            Console.WriteLine();
+            Console.WriteLine("Does Person 1 make more money than Person 2?");
+            bool comparison = person1Annual > person2Annual;
+            Console.WriteLine(comparison);
+            Console.ReadLine();
+        }
+    }
+}
