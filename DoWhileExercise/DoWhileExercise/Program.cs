421079b0ff51dfe88e9540e3d186b2a2e3ed610b
Anthony-Glatt <40805126+Anthony-Glatt@users.noreply.github.com>
2018-07-31 12:22:46 -0700

    Practicing While and Do While

---
+++ DoWhileExercise/DoWhileExercise/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace DoWhileExercise
+{
+    class Program
+    {
+        static void Main() //2. Do a boolean comparison using a do while statement.
+        {
+            Console.WriteLine("What is the capital of Oregon?");
+            Console.WriteLine("a.Portland");
+            Console.WriteLine("b.Salem");
+            Console.WriteLine("c.Eugene");
+            Console.WriteLine("d.Newport");
+            string choice = Console.ReadLine();
+
+            do
+            {
+                Console.WriteLine("Enter your choice.");
+
+                
+
+                if ( Console.ReadLine().StartsWith("b"))
+                {
+                    Console.WriteLine("Correct!");
+                    Console.ReadLine();
+                    return;
+                }
+                else Console.WriteLine("Not quite.");
+            }
+            while (!Console.ReadLine().StartsWith("b"));
+            Console.ReadLine();
+        }
+    }
+} 
---
+++ WhileExercise/WhileExercise/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+namespace WhileExercise
+{
+    class Program
+    {
+        static void Main()       //1. Do a boolean comparison using a while statement.
+        {
+            Console.WriteLine("Guess a number between 1 - 10");
+            int guess = Convert.ToInt32(Console.ReadLine());
+            Random getRandom = new Random();
+            int answer = getRandom.Next(0, 11);
+            bool isGuessed = guess == answer;
+
+            while (!isGuessed)
+            {
+                Console.WriteLine("Try Again.");
+                guess = Convert.ToInt32(Console.ReadLine());
+
+                if (answer == guess)
+                {
+                    Console.WriteLine("Congratulations, you guessed the number!");
+                    Console.ReadLine();
+                    return;
+                }
+            }
+        }
+    }
+}
