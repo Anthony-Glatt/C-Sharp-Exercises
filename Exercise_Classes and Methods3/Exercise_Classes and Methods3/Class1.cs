11f4d35c8a51fb251124bcae03a2882860be031c
Anthony-Glatt <40805126+Anthony-Glatt@users.noreply.github.com>
2018-08-03 16:35:48 -0700

    Fixed some instantiation issues

--- Exercise_Classes and Methods3/Exercise_Classes and Methods3/Class1.cs
+++ Exercise_Classes and Methods3/Exercise_Classes and Methods3/Class1.cs
@@ -6,10 +6,11 @@ namespace Exercise_Classes
 {
     class Class1
     {
-        public static void Method1(int  num01, int num02 = 0)
+        public int Method1(int  num01, int num02 = 0)
         {
             // 1. Create a class. In that class, create a method that takes two integers as parameters. Make one of them optional. Have the method do a math operation and return a integer result.
-            Console.WriteLine("The addition of " + num01 + " and " + num02 + " equals: " + (num01 + num02));
+            int result = num01 + num02;
+            return result;
         }
     }
 }
--- Exercise_Classes and Methods3/Exercise_Classes and Methods3/Program.cs
+++ Exercise_Classes and Methods3/Exercise_Classes and Methods3/Program.cs
@@ -8,8 +8,7 @@ namespace Exercise_Classes_and_Methods3
         static void Main(string[] args)
         {
             // 2. In the Main() method of the console app, instantiate the class.
-            Class1.Method1(5, 5);
-            Console.ReadLine();
+            Class1 addition = new Class1();
 
             //3. Ask the user to input two numbers, one at a time. Let them know they need not enter anything for the second number.
             Console.WriteLine("Please enter a number.");
@@ -17,16 +16,18 @@ namespace Exercise_Classes_and_Methods3
             Console.WriteLine("If you would like, please enter in another number.");
             string userNum2 = Console.ReadLine();
 
-            //4. Call the met hod in the class, passing in the one or two numbers entered.
+            //4. Call the method in the class, passing in the one or two numbers entered.
             if (userNum2 == "")
             {
-                Class1.Method1(userNum1);
+                int result1 = addition.Method1(userNum1);
+                Console.WriteLine(userNum1 + " + 0 = " + result1);
                 Console.ReadLine();
             }
             else
             {
                 int userNumber2 = Convert.ToInt32(userNum2);
-                Class1.Method1(userNum1, userNumber2);
+                int result2 = addition.Method1(userNum1, userNumber2);
+                Console.WriteLine(userNum1 + " + " + userNumber2 + " = " + result2);
                 Console.ReadLine();
             }
             
