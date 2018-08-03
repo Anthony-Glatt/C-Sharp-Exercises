7cd53630c61e3d2ce9b7f135d3f7d9cc7f1d0738
Anthony-Glatt <40805126+Anthony-Glatt@users.noreply.github.com>
2018-08-02 17:31:51 -0700

    Practice with classes and methods

---
+++ Exercise_Classes and Methods/Exercise_Classes and Methods/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exercise_Classes_and_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number to have math operations performed on it.");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            Exercise_Classes.Class1.Method1(userInput);
+            
+
+            Exercise_Classes.Class1.Method2(userInput);
+            
+
+            Exercise_Classes.Class1.Method3(userInput);
+            Console.ReadLine();
+        }
+    }
+}
