657317b5fb34749b4034954d36c75c5a97faaf4c
Anthony-Glatt <40805126+Anthony-Glatt@users.noreply.github.com>
2018-08-03 12:31:19 -0700

    More practice with methods and classes

---
+++ Exercise_Classes and Methods2/Exercise_Classes and Methods2/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise_Classes
+{
+    class Class1
+    {
+        // 1. Create a class. In that class, create a method that will take in an integer, do a math operation to it and then return the answer as an integer.
+        public static void Method1(int num01)
+        {
+            Console.WriteLine("Your number added to 20 equals: " + (num01 + 20));
+        }
+
+        //3. Add a second method to the class, with the same name, that will take in a decimal, do a different math operation to it and then return the answer as an integer.
+        public static void Method1(decimal num01)
+        {
+            Console.WriteLine("Your number divided by 10 equals: " + (num01 / 10));
+        }
+
+        //5. Add a third method to the class, with the same name, that will take in a string, convert it to an integer if possible, do a different math operation to it and then return the answer as an integer.
+        public static void Method1(string num01)
+        {   int num02 = Convert.ToInt32(num01);
+            Console.WriteLine("Your number mutliplied by 10 equals: " + (num02 * 10));
+        }
+    }
+}
---
+++ Exercise_Classes and Methods2/Exercise_Classes and Methods2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Exercise_Classes;
+
+namespace Exercise_Classes_and_Methods2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 2.In the Main() method of the console app, instantiate the class and call the one method, passing in an integer, such as 12. Then display the result to the screen.
+            Class1.Method1(12);
+            Console.ReadLine();
+
+            //4. In the Main() method of the console app, instantiate the class and call the second method, passing in a decimal. Display the result to the screen.
+            Class1.Method1(.5M);
+            Console.ReadLine();
+
+            //6. In the Main() method of the console app, instantiate the class and call the third method, passing in a string that equates to an integer. Display the result to the screen.
+            Class1.Method1(8);
+            Console.ReadLine();
+        }
+    }
+}
