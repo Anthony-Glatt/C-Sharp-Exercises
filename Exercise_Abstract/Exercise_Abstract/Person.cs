2f539d42d40d9a2340e0b240b81f52f9ecd5f7d4
Anthony-Glatt <40805126+Anthony-Glatt@users.noreply.github.com>
2018-08-06 16:59:12 -0700

    Everything should be working now

---
+++ Exercise_Abstract/Exercise_Abstract/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_Abstract
+{
+    public abstract class Person
+    {
+        //1. Create an abstract class called Person with two properties: string firstName and string lastName.
+        public string firstName;
+        public string lastName;
+
+        //2. Give it the method SayName().
+        public void SayName()
+        {
+            Console.WriteLine("Name: " + firstName + " " + lastName);
+        }
+    }
+}
