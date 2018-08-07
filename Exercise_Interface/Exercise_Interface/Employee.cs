7895163f7fa5d0a1d5b9968c24c9a5c4a2e173dd
Anthony-Glatt <40805126+Anthony-Glatt@users.noreply.github.com>
2018-08-06 17:56:58 -0700

    Practice with Interfaces

---
+++ Exercise_Interface/Exercise_Interface/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_Interface
+{
+    //2. Have your Employee class from the previous drill inherit that interface and implement the Quit() method in any way you choose.
+    class Employee : IQuittable
+    {
+        public void Quit()
+        {
+            Console.WriteLine("Hello world");
+        }
+
+        //3. Use polymorphism to create an object of type IQuittable and call the Quit() method on it. Hint: an object can be of an interface type if it implements that specific interface.
+        
+    }
+}
