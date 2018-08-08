bc84347c72ce09d26226a9e102adb6e4dc85d4e3
Anthony-Glatt <40805126+Anthony-Glatt@users.noreply.github.com>
2018-08-08 11:25:44 -0700

    Practice with Structs

---
+++ Exercise_Structs/Exercise_Structs/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_Structs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //2. In the Main() method, create an object of data type Number and assign an amount to it.
+            Number money = new Number();
+            money.Amount = 1000000;
+
+            //3. Print this amount to the console.
+            Console.WriteLine("Amount equals " + money.Amount);
+            Console.ReadLine();
+        }
+    }
+}
