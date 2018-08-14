cbb2188980b82fe39a025d2ecd4aae93e33de2d8
Anthony-Glatt <40805126+Anthony-Glatt@users.noreply.github.com>
2018-08-13 17:35:20 -0700

    Practice with const, var, and chaining constructors

---
+++ Exercise_Additional Features/Exercise_Additional Features/FavoriteNumber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_Additional_Features
+{
+    class FavoriteNumber
+    {
+        //3. Chain two constructors together.
+        //string userName = { get;  set; }
+
+        public FavoriteNumber(string name) : this(name, 7)
+        {
+        }
+
+        public FavoriteNumber(string name, int number)
+        {
+            userName = name;
+            userNumber = number;
+        }
+        public string userName {get; set;}
+        public int userNumber { get; set; }
+
+    }
+}
---
+++ Exercise_Additional Features/Exercise_Additional Features/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_Additional_Features
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. Create a const variable
+            const string schoolName = "The Tech Academy";
+
+            //2.Create a variable using the keyword "var".
+            var startDate = "06/12/18";
+
+           
+            Console.WriteLine("Please enter your name.");
+            string name = Console.ReadLine();
+            Console.WriteLine("Please enter your favorite number.");
+            int number = Convert.ToInt32(Console.ReadLine());
+            FavoriteNumber object1 = new FavoriteNumber(name, number);
+            FavoriteNumber object2 = new FavoriteNumber(name);
+            Console.WriteLine(object1.userName +" is the name " + object1.userNumber + " is the number");
+            Console.WriteLine(object2.userName + " is the same name " + object2.userNumber + " is the other number");
+            Console.ReadLine();
+        }
+    }
+}
